33b9527789ecf06d1c413e7950bf31d80acdf3d3
ahmad <ahmad.shakla>
2020-06-19 16:13:44 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/ex4_os.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4_os.dir/PhysicalMemory.cpp.o"
+  "CMakeFiles/ex4_os.dir/SimpleTest.cpp.o"
+  "CMakeFiles/ex4_os.dir/VirtualMemory.cpp.o"
+  "ex4_os.pdb"
+  "ex4_os.exe"
+  "libex4_os.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4_os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "good bye, World!" << std::endl;
+    return 0;
+}
